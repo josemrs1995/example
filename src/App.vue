ff6b8997a2f111e5aab4d4687592e6fceb98cb15
Your josemrs1995 <josemrs1995@gmail.com>
2018-05-25 09:35:30 -0400

    violento

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">  
     <router-link to="/ejercicio1">ejercicio</router-link>
+     <router-link to="/ejercicio2">ejercicio2</router-link>
     <router-view/>
 
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import ejercicio1 from '@/components/ejercicio1'
+import ejercicio2 from '@/components/ejercicio2'
+import lista from '@/components/lista'
+import editar from '@/components/editar'
 
 Vue.use(Router)
 
@@ -16,6 +19,21 @@ export default new Router({
       path: '/ejercicio1',
       name: 'ejercicio1',
       component: ejercicio1
+    },
+    {
+      path: '/ejercicio2',
+      name: 'ejercicio2',
+      component: ejercicio2
+    },
+    {
+      path: '/lista',
+      name: 'lista',
+      component: lista
+    },
+    {
+      path: '/editar',
+      name: 'editar',
+      component: editar
     }
   ]
 })
